e00c6d60842ef3d47b9fa18e37a1a56159013913
Andrea <andrea.diblasix@gmail.com>
2018-10-03 15:23:06 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const R = require('ramda');
+
+const reverseSentence = (string) => {
+    let lengthGreather5 = R.compose(R.lt(5), R.length);
+
+    let reverseWords = R.cond([
+        [lengthGreather5, R.reverse],
+        [R.T, R.identity]
+    ]);
+
+    return R.join(' ',  R.map(reverseWords, R.split(' ', string)));
+}
+
+console.log(reverseSentence("this is testtest"));
